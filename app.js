27a20aa8fd0e3a9e22165fbf665c076951ee67ef
zhoufangbing <zhoufangbing@daojia-inc.com>
2020-09-11 13:50:13 +0800

    chore: init

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = require('koa')()
+  , logger = require('koa-logger')
+  , json = require('koa-json')
+  , views = require('koa-views')
+  , onerror = require('koa-onerror')
+  , path = require('path');
+var index = require('./src/routes/index');
+var users = require('./src/routes/users');
+
+// error handler
+onerror(app);
+
+// global middlewares
+app.use(views(path.join(__dirname, 'src', 'views'), {
+  extension: 'ejs'
+}));
+app.use(require('koa-bodyparser')());
+app.use(json());
+app.use(logger());
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(require('koa-static')(path.join(__dirname, 'src', 'public')));
+
+// routes definition
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app;
